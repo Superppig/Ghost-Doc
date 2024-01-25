b963e01c8706ad71cbc88f81ad1ce996800cfee9
1657125180@qq.com <1657125180@qq.com>
2024-01-25 15:10:19 +0800

    完善Player的Inspector

--- Assets/Scripts/Editor/PlayerEditor.cs
+++ Assets/Scripts/Editor/PlayerEditor.cs
@@ -9,6 +9,11 @@ public class PlayerEditor : AutoEditor
 {
     private const float WordWidth = 80f;
     private const float BlockWidth = 20f;
+    private const float Offset = 1f;
+
+    private Vector2 nameSize;
+    private bool foldout;
+    EStateType[] stateNames;
 
     private Player player;
     [AutoProperty]
@@ -19,51 +24,61 @@ public class PlayerEditor : AutoEditor
         base.OnEnable();
         player = target as Player;
         appendInspector = true;
+        nameSize = new Vector2(WordWidth, BlockWidth);
+        stateNames = (EStateType[])Enum.GetValues(typeof(EStateType));
+        foldout = true;
     }
 
     protected override void MyOnInspectorGUI()
     {
-        EditorGUILayout.Space();
+        Rect GetControlRect(float width, float height)
+            => EditorGUILayout.GetControlRect(GUILayout.Width(width), GUILayout.Height(height));
 
-        EditorGUILayout.LabelField("状态邻接矩阵", EditorStyles.boldLabel);
-
-        // 获取 Enum 类型的所有值
-        EStateType[] states = (EStateType[])Enum.GetValues(typeof(EStateType));
-
-        EditorGUILayout.BeginHorizontal(); // 开始第一行
-
-        GUILayout.Space(WordWidth);
-
-        // 显示列名
-        for (int i = 0; i < states.Length; i++)
+        foldout = EditorGUILayout.BeginFoldoutHeaderGroup(foldout, "状态邻接矩阵");
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        if (foldout)
         {
-            EditorGUILayout.LabelField(states[i].ToString(), GUILayout.Width(BlockWidth));
-        }
-
-        EditorGUILayout.EndHorizontal(); // 结束第一行
-
-        // 显示二维数组的状态切换关系
-        for (int i = 0; i < states.Length; i++)
-        {
-            EditorGUILayout.BeginHorizontal();
-
-            // 显示行名
-            EditorGUILayout.LabelField(states[i].ToString(), GUILayout.Width(WordWidth));
-
-            for (int j = 0; j < states.Length; j++)
+            GUIStyle wordStyle = new GUIStyle(GUI.skin.label)
             {
-                player.changeMatrix[i, j] = EditorGUILayout.Toggle(
-                    player.changeMatrix[i, j],
-                    GUILayout.Width(BlockWidth)
-                );
+                alignment = TextAnchor.MiddleRight,
+                fixedWidth = WordWidth,
+                fixedHeight = BlockWidth,
+            };
+            EditorGUI.indentLevel++;
+            // 显示列名
+            Matrix4x4 origin = GUI.matrix;
+
+            Rect columns = GetControlRect(WordWidth + BlockWidth * stateNames.Length, WordWidth);
+            Rect currentRect = new Rect(columns.min + new Vector2(0f, WordWidth - BlockWidth + Offset), nameSize);
+            for (int i = 0; i < stateNames.Length; i++)
+            {
+                GUIUtility.RotateAroundPivot(90f, currentRect.max);
+                EditorGUI.LabelField(currentRect, stateNames[i].ToString(), wordStyle);
+                GUI.matrix = origin;
+                currentRect.x += BlockWidth;    //注意不能用xMin属性
             }
 
-            EditorGUILayout.EndHorizontal();
-        }
+            GUI.matrix = origin;
+            // 显示行
+            for (int i = 0; i < stateNames.Length; i++)
+            {
+                Rect row = GetControlRect(columns.width, BlockWidth);
+                currentRect = new Rect(row.min, nameSize);
+                EditorGUI.LabelField(currentRect, stateNames[i].ToString(), wordStyle);
+                currentRect = new Rect(currentRect.min + new Vector2(WordWidth, 0f), currentRect.size);
+                for (int j = 0; j < stateNames.Length; j++)
+                {
+                    player.changeMatrix[i, j] = EditorGUI.Toggle(currentRect, player.changeMatrix[i, j]);
+                    currentRect.x += BlockWidth;
+                }
+            }
 
-        if (GUILayout.Button("Save"))
-        {
-            EditorUtility.SetDirty(player);
+            if (GUILayout.Button("Save"))
+            {
+                EditorUtility.SetDirty(player);
+            }
+
+            EditorGUI.indentLevel--;
         }
     }
 }
